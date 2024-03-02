89f743ba4384fa64ca1ba7bf77017e04262e184d
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-01 20:39:14 -0500

    Add: Add Exceptions

---
+++ GroupLucky/GroupLucky.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace GroupLucky.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public IEnumerable<KeyValuePair<string, string>> Errors { get; }
+        public ValidationException() : base("One or more validation errors occurred")
+        {
+            Errors = new List<KeyValuePair<string, string>>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures.Select(f => new KeyValuePair<string, string>(f.PropertyName, f.ErrorMessage));
+        }
+
+        public ValidationException(IEnumerable<KeyValuePair<string, string>> errorList) : this()
+        {
+            Errors = errorList;
+        }
+    }
+}
