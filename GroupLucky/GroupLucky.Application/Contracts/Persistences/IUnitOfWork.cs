18f2f8902bc6813bd4e35d3fb93cbfd98a139625
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-01 20:25:19 -0500

    Add: Implementing Interface IUnitOfWork

---
+++ GroupLucky/GroupLucky.Application/Contracts/Persistences/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace GroupLucky.Application.Contracts.Persistences
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> Complete();
+        Task<int> ExecStoreProcedure(string sql, params object[] parameters);
+        Task Rollback();
+        Task Commit();
+        Task BeginTransaction();
+    }
+}
